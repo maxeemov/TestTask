3ccfce82ab4828d08c755e94746341f77f58910a
Maxeemov Oleg <maxeemov@gmail.com>
2017-09-15 05:27:40 +0600

    task_fourth

--- Task4/backend.php
+++ Task4/backend.php
@@ -4,13 +4,13 @@ $get= $_POST["search"];
 $subject = $_POST["comment"];
 
 $string1= preg_replace('/\"(.*?)\"/' ,'', $get);//dellet strings in quotes
-$string1=rtrim ($string1);//delete spase of string end
-$array = preg_split("/\s+/", $string1); //split the srting
+$string1=rtrim ($string1);//delete spase of string1 end
+$array = preg_split("/\s+/", $string1); //split the srting1
 
  
 $string2 = preg_replace('/((.*) "|" (.*))/' ,'', $get);//delete strings around quotes
 $string2 = str_replace('"' ,'', $string2);//delete quots
-$string2=rtrim ($string2);//delete spase of string end
+$string2=rtrim ($string2);//delete spase of string2 end
 array_push($array,$string2); //add string2 in to arraylist
 
 $array1=$array;
